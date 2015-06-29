0da6e89fec079023d7c62d676a51b38c903415a5
Salman <salman.karim@hotmail.com>
2015-06-29 18:56:44 +0100

    passes first test

--- app/app.rb
+++ app/app.rb
@@ -1,5 +1,14 @@
 require 'sinatra/base'
+require_relative './models/link'
 
 class Bookmarkmgr < Sinatra::Base
 
+  set :views, proc { File.join(root, '..', 'views') }
+
+  get '/links' do
+    @links = Link.all
+    erb :'links/index'
+  end
+
+  run! if app_file == $0
 end
