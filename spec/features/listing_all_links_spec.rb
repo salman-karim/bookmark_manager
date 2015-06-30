0197ad47b0fea89f5ecaf6b5c3e5dfa39c4fe4ac
Salman <salman.karim@hotmail.com>
2015-06-30 10:27:50 +0100

    fixes first test

--- spec/features/listing_all_links_spec.rb
+++ spec/features/listing_all_links_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 feature 'User browses the list of links' do
 
   scenario 'when visiting the links page' do
-    Link.new(url: 'http://www.makersacademy.com', title: 'Makers Academy')
+    Link.create(url: 'http://www.makersacademy.com', title: 'Makers Academy')
 
     visit '/links'
     expect(page.status_code).to eq 200
